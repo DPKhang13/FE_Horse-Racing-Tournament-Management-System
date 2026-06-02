d39d8f156370ba190d304d317da8b2ae1c2364ab
Le Nguyen Hoang Tan <hoangtan210505@gmail.com>
2026-06-02 13:08:46 +0700

    horse-management-UI-page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import LandingPage from '../pages/LandingPage';
 import SchedulePage from '../pages/SchedulePage';
 import ResultsPage from '../pages/ResultsPage';
 import AuthPage from '../pages/AuthPage';
+import HorseManagementPage from '../pages/HorseManagementPage';
 import MainLayout from '../components/MainLayout';
 
 const AppRoutes = () => {
@@ -15,6 +16,7 @@ const AppRoutes = () => {
       <Route path="/" element={<MainLayout><LandingPage /></MainLayout>} />
       <Route path="/schedule" element={<MainLayout><SchedulePage /></MainLayout>} />
       <Route path="/results" element={<MainLayout><ResultsPage /></MainLayout>} />
+      <Route path="/horses" element={<MainLayout><HorseManagementPage /></MainLayout>} />
     </Routes>
   );
 };
