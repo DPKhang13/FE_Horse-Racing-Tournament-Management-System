76bf3f5d802fdbd98d662ec1a52995098841acd5
Le Nguyen Hoang Tan <hoangtan210505@gmail.com>
2026-07-01 00:25:50 +0700

    api-horse-management-admin

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -47,7 +47,7 @@ const AppRoutes = () => {
       <Route path="/results" element={protectedPage(<RaceResultList />)} />
       <Route path="/results/rankings" element={protectedPage(<RankingPage />)} />
       <Route path="/results/:resultId" element={protectedPage(<RaceResultDetail />)} />
-      <Route path="/horses" element={protectedPage(<HorseManagementPage />, ['horse_owner'])} />
+      <Route path="/horses" element={protectedPage(<HorseManagementPage />, ['admin', 'horse_owner'])} />
       <Route path="/spectator-dashboard" element={protectedPage(<SpectatorDashboard />, ['spectator'])} />
       <Route path="/prediction" element={protectedPage(<PredictionPage />, ['spectator'])} />
       <Route path="/tracking" element={protectedPage(<ResultTrackingPage />, ['spectator'])} />
